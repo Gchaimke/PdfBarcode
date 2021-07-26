2181fa16d1dfdb7e4361565898db87efd4eb79db
Chaim <gchaimke@gmail.com>
2021-07-26 18:30:22 +0300

    add pdf preview

--- MainFrame.py
+++ MainFrame.py
@@ -1,22 +1,19 @@
 # -*- coding: UTF-8 -*-
+import fitz
 import gettext
 import wx
 from pyzbar import pyzbar
 from pdf2image import convert_from_path
 from os.path import join
-from shutil import move
+from shutil import move, copyfile
 import tempfile
 import cv2
 import re
 from datetime import datetime
-from threading import Timer
 from pathlib import Path
 import configparser
+import threading
 
-# begin wxGlade: dependencies
-# end wxGlade
-
-# begin wxGlade: extracode
 poppler_path = r"poppler_21_03_0"
 config = configparser.ConfigParser()
 
@@ -37,6 +34,8 @@ else:
 	config.read('config.ini')
 
 config = config['default']
+
+
 # end wxGlade
 
 
@@ -66,8 +65,7 @@ class MainFrame(wx.Frame):
 		self.sizer_1 = wx.GridBagSizer(1, 1)
 		self.sizer_1.SetMinSize(700, 600)
 		
-		self.splitter_window = wx.SplitterWindow(self, wx.ID_ANY,
-		                                         style=wx.SP_3DBORDER | wx.SP_LIVE_UPDATE | wx.SP_PERMIT_UNSPLIT)
+		self.splitter_window = wx.SplitterWindow(self, wx.ID_ANY, style=wx.SP_3DBORDER | wx.SP_LIVE_UPDATE | wx.SP_PERMIT_UNSPLIT)
 		self.splitter_window.SetMinimumPaneSize(400)
 		self.splitter_window.SetSashGravity(0.5)
 		self.sizer_1.Add(self.splitter_window, (0, 0), (1, 1), wx.EXPAND, 0)
@@ -102,25 +100,18 @@ class MainFrame(wx.Frame):
 		hbox_lists = wx.BoxSizer(wx.HORIZONTAL)
 		vbox_recognition.Add(hbox_lists, 1, wx.ALL | wx.EXPAND, 5)
 		
-		self.list_not_recognized = wx.ListCtrl(self.recognition_pane, wx.ID_ANY,
-		                                       style=wx.FULL_REPAINT_ON_RESIZE | wx.LC_HRULES | wx.LC_REPORT
-		                                             | wx.LC_VRULES)
+		self.list_not_recognized = wx.ListCtrl(self.recognition_pane, wx.ID_ANY, style=wx.FULL_REPAINT_ON_RESIZE | wx.LC_HRULES | wx.LC_REPORT | wx.LC_VRULES)
 		self.list_not_recognized.SetBackgroundColour(wx.Colour(255, 201, 205))
 		self.list_not_recognized.AppendColumn(_("Not Recognized"), format=wx.LIST_FORMAT_LEFT, width=200)
-		self.list_not_recognized.AppendColumn("", format=wx.LIST_FORMAT_LEFT, width=0)
-		self.list_not_recognized.AppendColumn("", format=wx.LIST_FORMAT_LEFT, width=0)
 		hbox_lists.Add(self.list_not_recognized, 1, wx.ALL | wx.EXPAND, 5)
 		
-		self.list_recognized = wx.ListCtrl(self.recognition_pane, wx.ID_ANY,
-		                                   style=wx.LC_HRULES | wx.LC_REPORT | wx.LC_VRULES)
+		self.list_recognized = wx.ListCtrl(self.recognition_pane, wx.ID_ANY, style=wx.LC_HRULES | wx.LC_REPORT | wx.LC_VRULES)
 		self.list_recognized.SetBackgroundColour(wx.Colour(196, 255, 216))
 		self.list_recognized.AppendColumn(_("Recognized"), format=wx.LIST_FORMAT_LEFT, width=200)
-		self.list_recognized.AppendColumn("", format=wx.LIST_FORMAT_LEFT, width=0)
-		self.list_recognized.AppendColumn("", format=wx.LIST_FORMAT_LEFT, width=0)
 		hbox_lists.Add(self.list_recognized, 1, wx.ALL | wx.EXPAND, 5)
 		
-		self.list_selection = wx.ListCtrl(self.recognition_pane, wx.ID_ANY,
-		                                  style=wx.LC_HRULES | wx.LC_ICON | wx.LC_VRULES)
+		self.list_selection = wx.ListCtrl(self.recognition_pane, wx.ID_ANY, style=wx.LC_HRULES | wx.LC_REPORT | wx.LC_VRULES)
+		self.list_selection.AppendColumn(_("Selected"), format=wx.LIST_FORMAT_LEFT, width=400)
 		vbox_recognition.Add(self.list_selection, 1, wx.ALL | wx.EXPAND, 10)
 		
 		hbox_save = wx.BoxSizer(wx.HORIZONTAL)
@@ -151,6 +142,7 @@ class MainFrame(wx.Frame):
 		
 		if config.get('language') != 'he':
 			self.SetLayoutDirection(wx.Layout_LeftToRight)
+		
 		self.Layout()
 		self.Centre()
 		
@@ -158,16 +150,61 @@ class MainFrame(wx.Frame):
 		self.Bind(wx.EVT_BUTTON, self.test, self.start_btn)
 		self.Bind(wx.EVT_BUTTON, self.test, self.save_btn)
 		self.Bind(wx.EVT_SIZING, self.resize_preview_pane)
-	# end wxGlade
+		self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.recognized_list_event, self.list_recognized)
+		self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.not_recognized_list_event, self.list_not_recognized)
+		self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.selection_list_event, self.list_selection)
+		self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.not_recognized_click, self.list_not_recognized)
+		self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.selected_click, self.list_selection)
 	
-	def test(self, event):
+	# end wxGlade
+	def not_recognized_list_event(self, event):
+		name = self.list_not_recognized.GetItemText(event.GetIndex())
+		self.preview_pdf("pdf/not_recognized/{}.pdf".format(name))
+	
+	def recognized_list_event(self, event):
+		name = self.list_recognized.GetItemText(event.GetIndex())
+		self.preview_pdf("pdf/recognized/{}.pdf".format(name))
 		self.txt_name.Clear()
-		self.txt_name.WriteText(str(self.preview_pane.GetSize()))
-		self.preview_image()
+		self.txt_name.WriteText(str(name))
+	
+	def selection_list_event(self, event):
+		name = self.list_selection.GetItemText(event.GetIndex())
+		self.preview_pdf("pdf/not_recognized/{}.pdf".format(name))
+	
+	def not_recognized_click(self, event):
+		text = self.list_not_recognized.GetItemText(event.GetIndex())
+		self.list_not_recognized.DeleteItem(event.GetIndex())
+		self.list_selection.InsertItem(event.GetIndex(), text)
+	
+	def selected_click(self, event):
+		text = self.list_selection.GetItemText(event.GetIndex())
+		self.list_selection.DeleteItem(event.GetIndex())
+		self.list_not_recognized.InsertItem(event.GetIndex(), text)
+		
+	def test(self, event):
+		self.start_btn.Disable()
+		self.list_not_recognized.DeleteAllItems()
+		self.list_recognized.DeleteAllItems()
+		thread1 = threading.Thread(target=self.start)
+		thread1.start()
 	
 	def resize_preview_pane(self, event):
 		self.preview_image_size = self.preview_pane.GetSize()
 	
+	def preview_pdf(self, pdf):
+		doc = fitz.open(pdf)
+		print(doc.page_count)
+		pix = doc[0].get_pixmap()
+		if pix.alpha:
+			bitmap = wx.Bitmap.FromBufferRGBA(pix.width, pix.height, pix.samples)
+		else:
+			bitmap = wx.Bitmap.FromBuffer(pix.width, pix.height, pix.samples)
+		x, y = self.preview_image_size
+		bitmap = self.scale_bitmap(bitmap, x, y)
+		self.image_preview.SetBitmap(bitmap)
+		self.image_preview.Layout()
+		print("preview pdf")
+		
 	def preview_image(self):
 		image = wx.Image(self.preview_image_path)
 		bmp = wx.Bitmap(image)
@@ -175,7 +212,6 @@ class MainFrame(wx.Frame):
 		bmp = self.scale_bitmap(bmp, x, y)
 		self.image_preview.SetBitmap(wx.Bitmap(bmp))
 		self.image_preview.Layout()
-	
 	
 	@staticmethod
 	def scale_bitmap(bitmap, width, height):
@@ -197,9 +233,7 @@ class MainFrame(wx.Frame):
 	
 	def get_pdf_barcodes(self, pdf_file):
 		with tempfile.TemporaryDirectory() as path:
-			images = convert_from_path(pdf_file, dpi=600, fmt="jpeg", paths_only=True, thread_count=4,
-			                           output_folder=path,
-			                           poppler_path=poppler_path)
+			images = convert_from_path(pdf_file, dpi=600, fmt="jpeg", paths_only=True, thread_count=4, output_folder=path, poppler_path=poppler_path)
 			for image in images:
 				found = self.decode_barcodes(image)
 				if found is not None:
@@ -211,10 +245,19 @@ class MainFrame(wx.Frame):
 		
 		pdf_files = glob(config['input_folder'] + "\\*.pdf")
 		not_recognized_folder = config['input_folder'] + "\\not_recognized"
+		recognized_folder = config['input_folder'] + "\\recognized"
 		Path(not_recognized_folder).mkdir(parents=True, exist_ok=True)
+		Path(recognized_folder).mkdir(parents=True, exist_ok=True)
 		Path(config['output_folder']).mkdir(parents=True, exist_ok=True)
 		
+		guage_length = len(pdf_files)
+		print(guage_length)
+		self.progress_gauge.SetRange(guage_length)
+		progress = 0
+		recognized = 0
+		not_recognized = 0
 		for pdf in pdf_files:
+			progress += 1
 			now = datetime.now()
 			date_time = now.strftime("%d%m%y_%H_%M_%S")
 			new_name = '{}.pdf'.format(date_time)
@@ -223,9 +266,19 @@ class MainFrame(wx.Frame):
 			if barcode is not None:
 				print(pdf, " ", barcode)
 				new_name = '{}.pdf'.format(barcode)
-				move(pdf, join(config['output_folder'], new_name))
-				print(new_name)
+				# move(pdf, join(config['output_folder'], new_name))
+				copyfile(pdf, join(recognized_folder, new_name))
+				self.list_recognized.InsertItem(recognized, barcode)
+				print(barcode)
+				recognized += 1
 			else:
-				move(pdf, join(not_recognized_folder, new_name))
+				self.list_not_recognized.InsertItem(not_recognized, date_time)
+				print(date_time)
+				# move(pdf, join(not_recognized_folder, new_name))
+				copyfile(pdf, join(not_recognized_folder, new_name))
+				not_recognized += 1
+			
+			self.progress_gauge.SetValue(progress)
+		self.start_btn.Enable()
 
 # end of class MainFrame
